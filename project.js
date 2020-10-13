96691bf837a9c29b77e662d88bbb50711d971c1d
EugeneW9 <washingtonway89@gmail.com>
2020-10-13 16:12:21 -0400

    weekendproject

---
+++ project.js
@@ -0,0 +1,20 @@
+const covidChoices = ['learn new skill','start a business', 'invest', 'accept deafeat'];
+
+const moxieLevel = 1000
+const moxieLevel1 = 900
+const moxielevel2 = 700
+const moxieLevel3 = 500
+
+let userChoose = prompt.choice(
+  'What will you make of yourself? ${covidChoices}, choose at least 1');
+
+function choose(userChoose) {
+  for (let userScore(); userScore =< 500){
+    if (userScore == moxieLevel3){
+      console.log([3]);
+    } else if (usercore(); userscore =< 700){
+      console.log([2]);
+    } else if (userScore(); userscore =< 900){
+      console.log([1]);
+    }else if (userScore(); userScore =< 1000){
+      console.log([0]);
